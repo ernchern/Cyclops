3fc24b8b85324c507a98ccec7a3a06198af2fd99
Do Duc Huy <doduchuy@fit.cvut.cz>
2018-11-08 22:37:18 +0900

    First version of client_module

--- raspberry-module/src/main.py
+++ raspberry-module/src/main.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
 import requests, json
+import random 
 
 KEY_LENGTH = 8
 KEY_NAME = 'key.txt'
 SERVER_URL = "http://143.248.140.158:6780/bike/location"
+rand_lat = random.randint(-90,90)
+rand_long = random.randint(-180,180)
+
+
+def gps_simulator():
+    
+    return (rand_lat, rand_long) if random.randint(1,10)!=1 else (random.randint(-90,90),random.randint(-180,180))
+
+random.seed()
 
 key_file = open(KEY_NAME, 'r')
 key = key_file.read(KEY_LENGTH)
@@ -17,9 +27,11 @@ payload =   {
                 }
             }
 
-for x in range(5):
-    payload["location"]["lat"] = x
-    payload["location"]["long"] = 2*x
+for x in range(20):
+    
+    coordinates = gps_simulator()
+    payload["location"]["lat"] = coordinates[0]
+    payload["location"]["long"] = coordinates[1]
                 
     print("PAYLOAD:", payload)
 
