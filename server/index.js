4160edb7d58b9d0b71bcb5a385b6291046951456
moonhc <mhc3357@gmail.com>
2018-12-05 21:43:37 +0000

    Implement Android notification

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+let admin = require('firebase-admin');
+
+var serviceAccount = require('/home/cs459-ins-7/Cyclops/server/firebase_key.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
 let devices = {
     'qwerasdf': {
         memberId:'qwer',
@@ -26,6 +34,7 @@ let members = {
         passwd: 'qwer',
         name: 'qwer',
         key: ['qwerasdf'],
+        token: "",
     }
 };
 
@@ -53,7 +62,7 @@ app.get('/bike/location', (req, res) => {
 });
 
 app.post('/bike/location', (req, res) => {
-    console.log(req.body)
+    console.log('location', req.body)
     let params = req.body;
     if (!params["key"] || !params["location"]) {
         res.status(400).send('invalid format');
@@ -72,115 +81,155 @@ app.post('/bike/location', (req, res) => {
     if (!lastLocation) {
         result.alram = 0;
     }
-    else if ( ((lastLocation.lat - params["location"].lat) > 0 ||
-               (lastLocation.long- params["location"].long)> 0) &&
-                !device.bike.state) {
+    else if ( ((lastLocation.lat - params["location"].lat) != 0 ||
+               (lastLocation.long- params["location"].long)!= 0) &&
+                device.bike.state == 0) {
+        let token = members[device["memberId"]].token;
+        if (token) {
+            console.log(token);
+        let message = {
+            notification: {
+                title: 'WANRING!',
+                body: 'Your device is being stolen!'},
+            token: token,
+        };
+
+        admin.messaging().send(message)
+            .then((response) => {
+                    console.log('Successfully sent message:', response);
+                    })
+        .catch((error) => {
+                console.log('Error sending message:', error);
+                console.log('Value', members[device['memberId']]);
+                });
+        }
         result.alarm = 1;
     }
     else {
         result.alarm = 0;
     }
     result.state = device.bike.state;
+    console.log(result);
 
     res.status(200).json(result);
 });
 
 app.get('/bike/photo', (req, res) => {
 
-});
+        });
 
 app.put('/bike/state', (req, res) => {
-    console.log(req.body);
-    let params = req.body;
-    if (!params["key"] || !params["state"]) {
+        console.log("state", req.body);
+        let params = req.body;
+        if (!params["key"] || params["state"] == null) {
         res.status(400).send('invalid format');
         return;
-    }
+        }
 
-    let device = findDevice(params["key"]);
-    if (!hasBike) {
+        let device = findDevice(params["key"]);
+        if (!hasBike) {
         res.status(400).send('Cannot find entity');
         return;
-    }
+        }
 
-    device.bike.state = params["state"];
-    res.status(200).end();
-});
+        device.bike.state = params["state"];
+        console.log(device.bike);
+        res.status(200).end();
+        });
 
 app.get('/login', (req, res) => {
-    let params = req.query;
-    if (!params["member-id"] || !params["password"]) {
+        let params = req.query;
+        if (!params["member-id"] || !params["password"]) {
         res.status(400).send('invalid parameter');
         return;
-    }
-    
-    let member = checkLogin(params["member-id"], params["password"]);
-    if (!member) {
+        }
+
+        let member = checkLogin(params["member-id"], params["password"]);
+        if (!member) {
         res.status(400).send('Cannot find entity');
         return;
-    }
+        }
 
-    res.status(200).json(member);
-});
+        res.status(200).json(member);
+        });
 
 app.post('/member/register', (req, res) => {
-    let params = req.body;
-    if (!params["member-id"] || !params["password"] || !params["address"] || !params["name"]) {
+        let params = req.body;
+        if (!params["member-id"] || !params["password"] || !params["address"] || !params["name"]) {
         res.status(400).send('invalid format');
         return;
-    }
-    
-    if (params["member-id"] in members) {
+        }
+
+        if (params["member-id"] in members) {
         res.status(400).send('invalid format');
         return;
-    }
+        }
 
 
-    let member = {};
-    member.address = params["address"];
-    member.passwd = params["password"];
-    member.key = [];
-    member.name = params["name"];
-    members[params["member-id"]] = member;
+        let member = {};
+        member.address = params["address"];
+        member.passwd = params["password"];
+        member.key = [];
+        member.name = params["name"];
+        members[params["member-id"]] = member;
 
-    res.status(200).end();
+        res.status(200).end();
 });
 
 app.post('/bike/register', (req, res) => {
-    let params = req.body;
-    if (!params["member-id"] || !params["key"]) {
+        let params = req.body;
+        if (!params["member-id"] || !params["key"]) {
         res.status(400).send('invalid format');
         return;
-    }
+        }
 
-    if (!(params["member-id"] in members) ||
-        !(params["key"] in devices)) {
+        if (!(params["member-id"] in members) ||
+                !(params["key"] in devices)) {
         res.status(400).send('Cannot find entity');
         return;
-    }
-    
-    let device = findDevice(params["key"]);
-    let member = findMember(params["member-id"]);
-    if (hasBike(device)) {
+        }
+
+        let device = findDevice(params["key"]);
+        let member = findMember(params["member-id"]);
+        if (hasBike(device)) {
         res.status(400).send('invalid request');
         return;
-    }
+        }
 
-    device.memberId = params["member-id"];
-    device.bike = {
-        locations: [],
-        photoPath: '',
-        state: 0
-    }
+        device.memberId = params["member-id"];
+        device.bike = {
+locations: [],
+           photoPath: '',
+           state: 0
+        }
 
-    member.key.push(params["key"]);
+        member.key.push(params["key"]);
 
-    res.status(200).end();
+        res.status(200).end();
 });
+
+app.post('/auth/sendToken', (req, res) => {
+        console.log("sendToken", req.body);
+        let params = req.body;
+        if (!params["token"] || !params["member-id"]) {
+        res.status(400).send('invalid format');
+        return;
+        }
+
+        if (!(params["member-id"] in members)) {
+        res.status(400).send('Cannot find entity');
+        return;
+        }
+
+        let member = findMember(params["member-id"]);
+        member["token"] = params["token"];
+
+        res.status(200).end();
+        });
 
 app.listen(80, () => {
-	console.log('App running on port 80');
-});
+        console.log('App running on port 80');
+        });
 
 function findDevice(key) {
     return devices[key];
